4f44e4ff93b34eece10190cf78077eecb476d516
Yifeng Yuan <yuanyifeng@ufl.edu>
2026-03-04 21:40:33 +0800

    Update summary_meme.sh

--- summary_meme.sh
+++ summary_meme.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-#SBATCH -N 1                       # Number of nodes. You must always set -N 1 unless you receive special instruction from the system admin
-#SBATCH -n 1                       # Number of tasks. Don't specify more than 16 unless approved by the system admin
-#SBATCH --mail-type=END            # Type of email notification- BEGIN,END,FAIL,ALL. Equivalent to the -m option in SGE
-#SBATCH --mail-user=yuanyifeng@ufl.edu # Email to which notifications will be sent. Equivalent to the -M option in SGE. You must replace [] with your email address.
-#SBATCH -t 1-00:00:00
-#SBATCH --mem=200mb
-#########################################################################
 
-method=$1
+job=$1
 dep=$2
 ratio=$3
 
-for pl in 1 2; do
-  folder=pl"${pl}"_M"${method}"_top200_R2/meme_dep${dep}r${ratio}
-  f_sum=meme_pl${pl}_M${method}_dep${dep}r${ratio}_summary.txt
-  > $f_sum
+folder="${job}"_top200_R2/meme_dep${dep}r${ratio}
+f_sum=meme_pl${pl}_M${method}_dep${dep}r${ratio}_summary.txt
+> $f_sum
 
-  for txt in $(ls "${folder}"/*_meme.txt); do
-    gname=$(basename ${txt} | sed 's/_meme.txt//')
-    if grep -q 'E-value =' $txt; then
-      grep 'E-value =' $txt | xargs -i echo -e "${gname}\t{}" >> $f_sum
-    fi
-  done
+for txt in $(ls "${folder}"/*_meme.txt); do
+  gname=$(basename ${txt} | sed 's/_meme.txt//')
+  if grep -q 'E-value =' $txt; then
+    grep 'E-value =' $txt | xargs -i echo -e "${gname}\t{}" >> $f_sum
+  fi
 done
 
