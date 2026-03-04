50a057a818c0615facb1f966852567d4ab8efce2
Yifeng Yuan <yuanyifeng@ufl.edu>
2026-03-04 21:38:31 +0800

    Update meme.sh

--- meme.sh
+++ meme.sh
@@ -4,6 +4,7 @@
 job=$1
 dep=$2
 ratio=$3
+t=$4
 
 folder=${job}_top200_R2
 dir_o=${folder}/meme_dep${dep}r${ratio}
@@ -12,7 +13,7 @@ for file in $(ls "${folder}"/vcf_per_OTU/*_R_seq.txt); do
   gname=$(basename $file| sed 's/_R_seq.txt//')
   fas="${folder}"/vcf_per_OTU/${gname}_dep"${dep}"r${ratio}min8.fasta
 
-  meme -p 4 -dna -objfun classic -nmotifs 6 -mod zoops -evt 0.05 -time 3000 -minw 3 -maxw 4 -markov_order 0 -nostatus -oc ${dir_o} $fas
+  meme -p $t -dna -objfun classic -nmotifs 6 -mod zoops -evt 0.05 -time 3000 -minw 3 -maxw 4 -markov_order 0 -nostatus -oc ${dir_o} $fas
 
   mv ${dir_o}/meme.html ${dir_o}/${gname}_meme.html
   mv ${dir_o}/meme.txt ${dir_o}/${gname}_meme.txt
