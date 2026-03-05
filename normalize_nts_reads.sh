3d96343b4279ab3cd55c86ae93c1def4acda15a0
Yifeng Yuan <yuanyifeng@ufl.edu>
2026-03-05 15:20:21 +0800

    Update normalize_nts_reads.sh

--- normalize_nts_reads.sh
+++ normalize_nts_reads.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 job=$1
+nts=$2
+reads=$3
 
 for ratio in $(seq 0.1 0.1 1) ; do
-    python3 normalize_nts_reads.py --input ptsite_${job}_r${ratio}_depth.txt --output ptsite_${job}_r${ratio}_depth_normalized.txt --job ${job}
-    python3 normalize_nts_reads.py --input allsite_${job}_r${ratio}_depth.txt --output allsite_${job}_r${ratio}_depth_normalized.txt --job ${job}
+    python3 normalize_nts_reads.py --input ptsite_${job}_r${ratio}_depth.txt --output ptsite_${job}_r${ratio}_depth_normalized.txt --nts ${nts} --reads ${reads}
+    python3 normalize_nts_reads.py --input allsite_${job}_r${ratio}_depth.txt --output allsite_${job}_r${ratio}_depth_normalized.txt --nts ${nts} --reads ${reads}
 done
 
