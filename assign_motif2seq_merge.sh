95d50dba70aca02cf180a0cd0fd849962a70c373
Yifeng Yuan <yuanyifeng@ufl.edu>
2026-03-05 11:53:23 +0800

    Update and rename assign_motif2seq.sh to assign_motif2seq_merge.sh

--- assign_motif2seq_merge.sh
+++ assign_motif2seq_merge.sh
@@ -4,7 +4,7 @@ list=$1
 job=$2
 
 # 1. assign motif to pileups.
-while IFS=$'\t' read -r genome pipeline method motif; do
+while IFS=$'\t' read -r genome job motif; do
   # Skip empty lines or comments
   [[ -z "$genome" || "$genome" =~ ^[[:space:]]*# ]] && continue
 
@@ -13,8 +13,7 @@ while IFS=$'\t' read -r genome pipeline method motif; do
 
   # Optional: Print what was received
   echo "pipeline: $pipeline"
-  echo "method: $method"
-  echo "genome: $genome"
+  echo "job: $job"
   echo "Motifs: ${motif_array[*]}"
 
   # Call your actual processing script with:
@@ -30,7 +29,7 @@ done < ${list}
 # R strand : pt site = pos (col2) + start site (col7)
 # F strand : new pos = pos + (length($5) - $7 + 1)
 
-while IFS=$'\t' read -r genome pipeline method motif; do
+while IFS=$'\t' read -r genome job motif; do
   awk -F '\t' '{print $0"\t"$2+length($5)-$7+1}' "${job}"_top200_R2/vcf_per_OTU/${genome}_F_seq_motif.txt > "${job}"_top200_R2/vcf_per_OTU/${genome}_F_seq_motif_newsite.txt
   awk -F '\t' '{print $0"\t"$2+$7}' "${job}"_top200_R2/vcf_per_OTU/${genome}_R_seq_motif.txt > "${job}"_top200_R2/vcf_per_OTU/${genome}_R_seq_motif_newsite.txt
 done < list_genome_motif.txt
@@ -38,9 +37,15 @@ done < list_genome_motif.txt
 # 3. merge pileups distance <=3.
 # The input file is a tab delimited txt file with 8 columns. The 2nd, 3rd, 4th, 7th and 8th columns are numbers.
 # A python script that merges rows of the input file and save the output to a new tsv file. Between all rows have the same first column (strings), and when the absolute abstract of the two numbers in the 8th columns are less than 10, print the row between the two rows whose value in the 7th column is more close to 10 and replace the 4th columns with the sum of the 4th columns of the two rows. When the difference of the value in the 7th column of the two rows is tie, print the row of which the 4th column is larger. If the values in the 4th columns are still the same, print the first occurring row and replace the 4th columns with the sum of the 4th columns of the two rows.
-while IFS=$'\t' read -r genome pipeline method motif; do
+while IFS=$'\t' read -r genome job motif; do
   python3 merge_pileup.py "${job}"_top200_R2/vcf_per_OTU/${genome}_F_seq_motif_newsite.txt "${job}"_top200_R2/site/${genome}_F_seq_motif_merge.txt
   python3 merge_pileup.py "${job}"_top200_R2/vcf_per_OTU/${genome}_R_seq_motif_newsite.txt "${job}"_top200_R2/site/${genome}_R_seq_motif_merge.txt
+done < list_genome_motif.txt
+
+# 4. merge strand
+while IFS=$'\t' read -r genome job motif; do
+  awk -F '\t' '{print $0"\tR"}' "${job}"_top200_R2/site/${genome}_R_seq_motif_merge.txt > "${job}"_top200_R2/site/${genome}_allsite.txt
+  awk -F '\t' '{print $0"\tF"}' "${job}"_top200_R2/site/${genome}_F_seq_motif_merge.txt >> "${job}"_top200_R2/site/${genome}_allsite.txt
 done < list_genome_motif.txt
 
 ## END
